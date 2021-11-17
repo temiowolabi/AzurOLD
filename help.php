baa92f204688bc7710f5cf783ef24f4f4e73c875
Temi Owolabi <65958151+temiowolabi@users.noreply.github.com>
2021-11-17 08:06:16 +0000

    Added Terms of Service to Help Page

--- help.php
+++ help.php
@@ -19,7 +19,31 @@ include(ROOT_PATH . "/includes/header.php"); ?>
         <h5>Terms Of Service</h5>
     </div>
     <div class="panel">
-    <p>Come up with terms of service agreement</p>
+    <p>Depending on the laws applicable in your jurisdiction, you and/or your use of the Products and Services may be 
+       liable for certain obligations. When using our Products and Services, you agree that it is your responsibility, 
+       and not Azur's, to ensure that you comply with any applicable laws, including but not limited to: </p>
+
+       <p>
+       (1) Any laws or regulations that regulate the production, sharing or distribution of video or audio content,
+       </p>
+       <p>
+       (2) For example, laws or regulations requiring you to display appropriate signage advising others that audio/visual recordings are being made, or laws or regulations which require that notice be given to 
+           or consent be obtained from third parties regarding your use of the Products or Services.
+       </p>
+       <p>
+       (3) It must be installed at an angle that ensures it does not capture any recordings beyond the boundary of your property (including public paths or roads); and it must comply with laws governing monitoring 
+           of employees if it is used as a workplace.
+       </p>
+
+       <p>
+       You are not authorized to use the Services or Products if they are prohibited by applicable laws.
+       It's <strong>not</strong> our responsibility if you use the Services or any Products in a way that breaks the law.
+       </p>
+
+       <p>
+       A user name and password ("User ID") must be selected during registration. Your registration information must be accurate, complete, and up-to-date. User IDs should not be selected if you don't have the right to use them, or if you intend to impersonate another person.
+       In addition, you will not share your account or password with anyone, and you will keep your password and account secure. You are responsible for all activities that occur under your account.
+       </p>
     </div>
 
     <div class="accordion">
